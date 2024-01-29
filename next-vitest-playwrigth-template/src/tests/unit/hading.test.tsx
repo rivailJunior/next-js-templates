a0c349983ca3ab126253e228af349a9f8e841ce2
rivailJunior <rivail.rj@gmail.com>
2024-01-29 14:50:47 -0400

    fix: template vitest unit

--- next-vitest-playwrigth-template/src/tests/unit/hading.test.tsx
+++ next-vitest-playwrigth-template/src/tests/unit/hading.test.tsx
@@ -2,6 +2,12 @@ import { Heading } from "@/components";
 import { render, screen } from "@testing-library/react";
 import { describe, expect, test } from "vitest";
 
+vi.mock("@/config/env", () => ({
+  env: {
+    WELCOME_MESSAGE: "We are here to help you with your Next.js project 🥳",
+  },
+}));
+
 describe("Heading Component", () => {
   test("should render Heading Component properly", () => {
     render(<Heading />);
